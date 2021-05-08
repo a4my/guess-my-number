188b843fd708fb7c9357497e4e251f8641d209b8
Alex Fourmy <alexfourmy@gmail.com>
2021-05-08 11:08:35 +0200

    started worgking on the game logic

---
+++ app.js
@@ -0,0 +1,36 @@
+// const secretNumber = Math.trunc(Math.random() * 20) + 1;
+const secretNumber = 3
+
+let score = 20
+
+document.querySelector('.number').textContent = secretNumber
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value)
+  console.log(guess)
+
+  if(!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!'
+  } else if(guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct number!!'
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').style.width = '30rem'
+  } else if(guess > secretNumber) {
+      if(score > 1) {
+        document.querySelector('.message').textContent = '➕ Too high!'
+        score--
+        document.querySelector('.score').textContent = score
+      } else {
+        document.querySelector('.message').textContent = '💥 You lost the game, you sucker!'
+        document.querySelector('.score').textContent = 0
+      }
+  } else if(guess < secretNumber) {
+    if(score > 1) {
+      document.querySelector('.message').textContent = '➖ Too low!'
+      score--
+      document.querySelector('.score').textContent = score
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game, you sucker!'
+      document.querySelector('.score').textContent = 0
+    }
+  }
+})
